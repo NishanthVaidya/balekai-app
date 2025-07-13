f044a0bf71dea0a511b8103534fe0b48ea963597
NishanthVaidya <nishanthvaidya1998@gmail.com>
2025-07-13 13:35:14 -0400

    1

--- frontend/app/utils/token.tsx
+++
@@ -1,9 +0,0 @@
-// utils/token.ts
-export const getToken = () => {
-  if (typeof window === 'undefined') return null;
-  return localStorage.getItem('jwt');
-};
-
-export const clearToken = () => {
-  if (typeof window !== 'undefined') localStorage.removeItem('jwt');
-};
