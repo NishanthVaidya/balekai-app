93d2524831f210ef32d1b1753c219f6756e51dd7
NishanthVaidya <nishanthvaidya1998@gmail.com>
2025-07-30 17:36:20 -0400

    Update documentation and configuration for current deployment state

--- balekai-frontend/app/boards/[boardId]/test-page.tsx
+++
@@ -1,8 +0,0 @@
-export default function TestPage() {
-  return (
-    <div>
-      <h1>Test Dynamic Route</h1>
-      <p>This is a test page to verify dynamic routing works on Vercel.</p>
-    </div>
-  )
-} 
