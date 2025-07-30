52ee03183b28e53f532a622c46484fc886c5db3c
NishanthVaidya <nishanthvaidya1998@gmail.com>
2025-07-30 17:35:12 -0400

    Add test dynamic route to verify Vercel routing

---
+++ balekai-frontend/app/boards/[boardId]/test-page.tsx
@@ -0,0 +1,8 @@
+export default function TestPage() {
+  return (
+    <div>
+      <h1>Test Dynamic Route</h1>
+      <p>This is a test page to verify dynamic routing works on Vercel.</p>
+    </div>
+  )
+} 
