0ee528df905f837a34c28e968de0b16fd2fbbdec
NishanthVaidya <nishanthvaidya1998@gmail.com>
2025-09-06 18:45:47 -0400

    Removed extra Dont have an account? Sin Up message on the login page

--- balekai-frontend/app/login/page.tsx
+++ balekai-frontend/app/login/page.tsx
@@ -14,11 +14,6 @@ export default function LoginPage() {
       <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
 
         <LoginForm />
-        <p className="px-8 text-center text-sm text-muted-foreground">
-          <Link href="/register" className="hover:text-brand underline underline-offset-4">
-            Don&apos;t have an account? Sign up
-          </Link>
-        </p>
       </div>
     </div>
   )
