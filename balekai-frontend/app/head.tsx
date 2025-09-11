b27dc35baa42d4ff70eb52c3bffc3a86d2448ee1
NishanthVaidya <nishanthvaidya1998@gmail.com>
2025-09-11 13:57:29 -0400

    Title Change

---
+++ balekai-frontend/app/head.tsx
@@ -0,0 +1,11 @@
+export default function Head() {
+  return (
+    <>
+      <title>balekai</title>
+      <meta name="application-name" content="balekai" />
+      <meta name="description" content="balekai - The task management app" />
+    </>
+  )
+}
+
+
