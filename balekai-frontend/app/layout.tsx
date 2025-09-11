8ab21a3434438ec0770ba6356542caa0371e9b8c
NishanthVaidya <nishanthvaidya1998@gmail.com>
2025-09-11 16:47:47 -0400

    Title Change

--- balekai-frontend/app/layout.tsx
+++ balekai-frontend/app/layout.tsx
@@ -1,16 +1,17 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
+import type React from "react"
+import { Suspense } from "react"
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
-});
+})
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
-});
+})
 
 export const metadata: Metadata = {
   title: "balekai",
@@ -26,20 +27,18 @@ export const metadata: Metadata = {
     description: "balekai - The task management app",
     card: "summary_large_image",
   },
-};
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <Suspense fallback={<div>Loading...</div>}>{children}</Suspense>
       </body>
     </html>
-  );
+  )
 }
